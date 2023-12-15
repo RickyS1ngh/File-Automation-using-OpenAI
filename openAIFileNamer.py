c0f1e7d3b2b7cbc4b9797226af243b303da40c8a
Ricky Singh <Gurjinder.Singh.Ricky@protonmail.com>
2023-12-15 16:40:36 -0500

    Fix typo in content part of system

--- openAIFileNamer.py
+++ openAIFileNamer.py
@@ -9,7 +9,7 @@ def newFileName(fileName, fileContent):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a file naming assistant, skilled in naming files with new relevant and descriptive names that keeps the previous extension of the filegi. Please respond with only the file name."},
+            {"role": "system", "content": "You are a file naming assistant, skilled in naming files with new relevant and descriptive names that keeps the previous extension of the file. Please respond with only the file name."},
             {"role": "user", "content": f"Please suggest a new name of this file. The original file name is {fileName} and the contents of the file are {fileContent} "}
         ]
     )
