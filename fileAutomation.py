51f06173274b82cd0f4b3b5190f8b7e476f505b2
Ricky Singh <Gurjinder.Singh.Ricky@protonmail.com>
2023-12-18 15:36:33 -0500

    Format using autopep8

--- fileAutomation.py
+++ fileAutomation.py
@@ -10,26 +10,33 @@ def fileMove(file, targetPath, path):
         fileName = file.name
     else:
         fileName = file.split("/")[-1]
-    if (not os.path.exists(targetFolderPath)):  # Check if the target folder exists
+    if (not os.path.exists(targetFolderPath)
+        ):  # Check if the target folder exists
         os.makedirs(targetFolderPath)  # Creates the directory
-    targetFilePath= os.path.join(targetFolderPath, fileName)
-    if (not os.path.exists(targetFilePath) #Checks if the path exists with the file, if it doesnt, the file is moved
-        ):  # Checks if a file with the name exists in the directory
+    targetFilePath = os.path.join(targetFolderPath, fileName)
+    if (not os.path.exists(targetFilePath)  # Checks if the path exists with the file, if it doesnt, the file is moved
+            ):  # Checks if a file with the name exists in the directory
         shutil.move(file, targetFolderPath)  # moves file
         print(f"{fileName} has moved to {targetFolderPath}")
     else:
-        if os.stat(file).st_size == os.stat(targetFilePath).st_size: #Checks if the file in the target folder is the the same file. Removes the file if it is the same file
+        # Checks if the file in the target folder is the the same file. Removes
+        # the file if it is the same file
+        if os.stat(file).st_size == os.stat(targetFilePath).st_size:
             print(f"{fileName} already exists. It will be deleted")
             os.remove(file)
         else:
+            # Cannot move the file because another file with the same name
+            # exists
             print(
-                f"{fileName} cannot be moved because a file with the same name already exist")#Cannot move the file because another file with the same name exists
+                f"{fileName} cannot be moved because a file with the same name already exist")
 
 
 def fileAutomation():
-    userInput = input("Would you like openAI to assist in naming your files? Yes/No or Y/N: ").lower()
+    userInput = input(
+        "Would you like openAI to assist in naming your files? Yes/No or Y/N: ").lower()
     while userInput not in ("yes", "y", "no", "n"):
-        userInput = input("Input was invalid. Please enter Yes/No or Y/N").lower()
+        userInput = input(
+            "Input was invalid. Please enter Yes/No or Y/N").lower()
 
     directories = ["/Users/rickysingh/Downloads", "/Users/rickysingh/Desktop"]
     targetPath = "/Users/rickysingh/Documents"
@@ -58,7 +65,13 @@ def fileAutomation():
                     for ext, path in EXTS.items():
                         if extension == ext:
                             if ext not in (
-                                    "jpeg", "jpg", "mov", "mp4", "pptx") and userInput in ("yes", "y"):
+                                    "jpeg",
+                                    "jpg",
+                                    "mov",
+                                    "mp4",
+                                    "pptx") and userInput in (
+                                    "yes",
+                                    "y"):
                                 try:
                                     fileContents = contentExtracter(file, ext)
                                     newName = newFileName(
